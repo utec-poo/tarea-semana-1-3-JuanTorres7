4a12895f65627e69f14491b9f474163e7767ca33
JuanTorres7 <55298418+JuanTorres7@users.noreply.github.com>
2019-09-15 12:52:08 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main (){
+    int cant, pares = 0, impares = 0;
+    for (int i;; i++){
+        int num;
+        cout << "Ingrese numero " << i+1 << ":\n";
+        cin >> num;
+        cant = i;
+        if (num % 2 == 0){
+            pares++;
+        }
+        else if (num % 2 != 0){
+            impares++;
+        }
+        if (num == 0){
+            break;
+        }
+    }
+    cout << "La cantidad de numeros leidos fue de " << cant << "\n";
+    cout << "La cantidad de numeros pares son " << pares-1 << "\n";
+    cout << "La cantidad de numeros impares son " << impares << "\n";
+    return 0;
 }
