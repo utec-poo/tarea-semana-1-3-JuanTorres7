f2d750c0ff5f6532828772569a9846102ba2383d
JuanTorres7 <55298418+JuanTorres7@users.noreply.github.com>
2019-09-17 08:27:24 -0500

    Update main.cpp

--- ejercicio7/main.cpp
+++ ejercicio7/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
-
-entero numero;
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main (){
+    int i, n, num;
+    cout << "Input de numero entero: ";
+    cin >> num;
+    if (num > 0 && num <= 30){
+        for (i=1;i<=num;i++){
+            for(n=1; i>=n; n++)
+                cout << "*";
+            cout << "\n";
+        }
+    }
+    else{
+        cout << "El numero debe estar en el rango de 1 a 30.";
+    }
+    return 0;
 }
